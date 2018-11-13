cc0b1ccbc04a4529b661a35a8c7596b7b535ea3b
andrea.decastri <andrea.decastri@absontheweb.com>
2018-11-13 18:01:13 +0100

    Login errors moved below login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,17 +15,16 @@
             </div>
             <button id="button-submit-login" type="submit" class="btn btn-primary">@lang('auth.label-login')</button>
         </form>
-
-        @if ($errors->any())
-            <div id="panel-errors-login" class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
     </div>
+
+    @if ($errors->any())
+        <div id="panel-errors-login" class="alert alert-danger col-10 col-sm-8 col-md-6 col-lg-4 mr-auto ml-auto px-3 py-4 mt-3">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
 @endsection
