df49cd402229091eceb540426351ae537be351e1
andrea.decastri <andrea.decastri@absontheweb.com>
2018-11-13 16:20:33 +0100

    Added binding IAccountService to AccountService into container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
+        $this->app->bind('App\Services\Interfaces\IAccountService', 'App\Services\AccountService');
     }
 }
