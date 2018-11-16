d2fcf464a54f75c1693b53e40fe91ab3b1d4370e
giuseppe <giuseppe.saraceno@zanichelli.it>
2018-11-16 10:13:55 +0100

    deleted username  from Model

--- app/Models/Account/User.php
+++ app/Models/Account/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements JWTSubject {
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'name', 'surname', 'is_verified', 'password'
+        'email', 'name', 'surname', 'is_verified', 'password'
     ];
 
     /**
